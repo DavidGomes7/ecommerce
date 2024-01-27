611005fc302ecc928ab5b08ca600c8910b7de8a5
unknown <j.davidgms.dg@gmail.com>
2024-01-26 22:21:01 -0300

    Criando a classe PageAdmin e tpl admin

---
+++ vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -0,0 +1,70 @@
+<?php 
+
+namespace Hcode;
+
+
+class PageAdmin extends Page {
+
+	public function __construct($opts = array(), $tpl_dir = "/Udemy/PHP/ecommerce/views/admin/") {
+
+		parent::__contruct($opts, $tpl_dir);
+
+	}
+}
+
+
+// use \Rain\Tpl;
+
+// class PageAdmin {
+
+// 	private $tpl;
+// 	private $options = [];
+// 	private $defaults = [
+// 		"data"=>[]
+
+// 	];
+
+// 	public function __construct($opts = array(), $tpl_dir = "/Udemy/PHP/ecommerce/views/admin/"){
+
+// 		$this->options = array_merge($this->defaults, $opts);
+
+// 		$config = array(
+// 			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"] . $tpl_dir,
+// 			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"] . "/Udemy/PHP/ecommerce/views-cache/",
+// 			"debug"         => false
+// 		   );
+
+// 		Tpl::configure( $config );
+
+// 		$this->tpl = new Tpl();
+
+// 		$this->setData($this->options["data"]);
+
+// 		$this->tpl->draw( "header" );
+
+
+// 	}
+
+// 	private function setData($data = array()) {
+
+// 		foreach ($data as $key => $value) {
+// 			$this->tpl->assign($key, $value);
+// 		}
+// 	}
+
+// 	public function setTpl($name, $data = array(), $returnHTML = false){
+
+// 		$this->setData($data);
+
+// 		return $this->tpl->draw($name, $returnHTML);
+
+// 	}
+
+// 	public function __destruct(){
+
+// 		$this->tpl->draw("footer");
+
+// 	}
+// }
+
+ ?>
